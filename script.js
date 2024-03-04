adfa88ce4dddf01e9d9be1680e33e80c2b3b00c8
MrRoy17 <mrdeveloper00001@gmail.com>
2024-03-04 12:54:58 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+  var istatus = document.querySelector(".foxer")
+
+  var addfollow =document.querySelector(".foxer")
+   
+  
+
+  addfollow.addEventListener("click",function(){
+    istatus.innerHTML = "following"
+    setTimeout(function(){
+        istatus.innerHTML = "following"
+
+    },10000)
+  })
+  
+  
